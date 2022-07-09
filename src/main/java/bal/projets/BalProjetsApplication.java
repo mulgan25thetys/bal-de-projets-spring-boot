47ce78168fd93546596735eb67816df1cb60fae5
mulgan25thetys <mulganthetys.ngougbiamayele@esprit.tn>
2022-07-09 11:19:03 +0100

    premier depot

---
+++ src/main/java/bal/projets/BalProjetsApplication.java
@@ -0,0 +1,13 @@
+package bal.projets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalProjetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalProjetsApplication.class, args);
+	}
+
+}
